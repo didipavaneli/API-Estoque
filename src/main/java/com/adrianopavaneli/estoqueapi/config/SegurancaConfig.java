2fd5ff6a036f04ef402204409532b94075687bfd
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-08-22 13:06:20 -0300

    Correcao autenticacao

--- src/main/java/com/adrianopavaneli/estoqueapi/config/SegurancaConfig.java
+++ src/main/java/com/adrianopavaneli/estoqueapi/config/SegurancaConfig.java
@@ -1,19 +1,25 @@
 package com.adrianopavaneli.estoqueapi.config;
 
-import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+
+
+
+
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
+
+
 
 @EnableWebSecurity
 @EnableAuthorizationServer
 @EnableResourceServer
-public class SegurancaConfig extends WebSecurityConfigurerAdapter{
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("adriano").password("12345678").roles("ADMIN");
-        
-    }
-}
+@Configuration
+public class SegurancaConfig  {
+    
+}  
